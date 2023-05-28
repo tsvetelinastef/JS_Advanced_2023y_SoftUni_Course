fdaa185c9c40513cfbe469211f67f60bc67d3eda
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-05-29 00:17:39 +0300

    Update ex3_areaOfSquare

--- 01. reviewOfBasicsAndFundamentas/Basics/ex3_areaOfSuare.js
+++ 01. reviewOfBasicsAndFundamentas/Basics/ex3_areaOfSuare.js
@@ -5,7 +5,7 @@
 }
 
 squareArea(["5"]); 
-
+----------------------------------------------------------------
  function inchesToSm(input){
     let x = Number(input[0]);
     let sm = x * 2.54;
@@ -14,9 +14,58 @@ squareArea(["5"]);
 
 inchesToSm(["5"]);  // 12.7
 inchesToSm(["7"]); // 17.78  
-
+-----------------------------------------------------------------
 function greetingByName(name){
     console.log(`Hello, ${name}`);
 }
-greetingByName("Nikki"); */
+greetingByName("Nikki"); 
+------------------------------------------------------------------
+
+function numbersFrom1To100(){
+    for (let i = 1; i <= 100; i++) {
+        console.log(i);
+    }
+}
+numbersFrom1To100();
+------------------------------------------------------------------
+function numbersNTo1(input){
+    let n = Number(input[0]);
+
+    for (let i = n; i > 0; i--) {
+        console.log(i);
+    }
+} 
+
+numbersNTo1("2");
+numbersNTo1("3");
+numbersNTo1("5"); 
+-------------------------------------------------------------------
+function numbersFrom1ToN(input){
+    let n = Number(input[0]);
+
+    for(let i = 1; i <= n; i += 3){
+        console.log(i);
+    }
+}
+
+numbersFrom1ToN(["10"]); 
+-------------------------------------------------------------------
+function evenPowersOf2(input){
+    let n = Number(input[0]);
+
+    for (let i = 0; i <= n; i += 2) {
+        console.log(Math.pow(2, i));
+    }
+}
+evenPowersOf2(["3"]); 
+-------------------------------------------------------------------- 
+function  characterSequence(input){
+    let text = input[0];
+    for (let i = 0; i < text.length; i++) {
+        const element = text[i];
+        console.log(element);
+    }
+} 
+
+characterSequence(["softuni"]);  */
 
