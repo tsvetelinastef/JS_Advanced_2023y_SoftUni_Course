ac0717ee71e4f88da0ea0250a3daf472c49e5b87
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-03 10:01:32 +0300

    Updated ex4

--- 04. Exercise Syntax, Functions and Statements/ex04_previousDay.js
+++ 04. Exercise Syntax, Functions and Statements/ex04_previousDay.js
@@ -1,9 +1,10 @@
 function previousDay(year, month, day){
-     
+    let date = new Date (year, month - 1, day - 1);
+    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
 }
 
 previousDay(2016, 9, 30);
 // 2016-9-29
 
-previousDay(2016, 9, 30);
+previousDay(2015, 5, 10);
 // 2015-5-9
