1a3cc9bfe31fe258f948349e4964951c80ebae48
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-04 00:09:54 +0300

    Update ex05

---
+++ 04. Exercise Syntax, Functions and Statements/ex05_timeOfWalk.js
@@ -0,0 +1,10 @@
+function timeOfWalk(steps, meters, km) {
+    let distanceInMeters = steps * meters;
+    let speedMetersInSecond = distanceInMeters / 3.6;
+   
+    
+    console.log(`${}:${}:${}`);
+}
+
+timeOfWalk(4000, 0.60, 5);  // 00:32:48
+//timeOfWalk(2564, 0.70, 5.5); // 00:22:35
