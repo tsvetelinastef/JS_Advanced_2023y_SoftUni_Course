778c7b3f2ded9bdd7e90dd2b909ddb0f81047e96
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-04 20:39:52 +0300

    Update ex05_timeOfWalk.js

--- 04. Exercise Syntax, Functions and Statements/ex05_timeOfWalk.js
+++ 04. Exercise Syntax, Functions and Statements/ex05_timeOfWalk.js
@@ -1,9 +1,21 @@
 function timeOfWalk(steps, meters, km) {
     let distanceInMeters = steps * meters;
-    let speedMetersInSecond = distanceInMeters / 3.6;
-   
-    
-    console.log(`${}:${}:${}`);
+    let speedMetersInSecond = km / 3.6;
+    let time = distanceInMeters / speedMetersInSecond;
+    let res = Math.floor(distanceInMeters / 500);
+
+    let timeInMin = Math.floor(time / 60);
+	let timeInSec = Number((time - timeInMin * 60).toFixed());
+	let timeInH = Math.floor(time / 3600);
+
+	timeInMin += res;
+	timeInH += Math.floor(timeInMin / 60);
+	timeInMin = timeInMin % 60;
+
+	let formattedH = timeInH < 10 ? `0${timeInH}` : `${timeInH}`;
+	let formattedMin = timeInMin < 10 ? `0${timeInMin}` : `${timeInMin}`;
+	let formattedSec = timeInSec < 10 ? `0${timeInSec}` : `${timeInSec}`;
+	console.log(`${formattedH}:${formattedMin}:${formattedSec}`);
 }
 
 timeOfWalk(4000, 0.60, 5);  // 00:32:48
