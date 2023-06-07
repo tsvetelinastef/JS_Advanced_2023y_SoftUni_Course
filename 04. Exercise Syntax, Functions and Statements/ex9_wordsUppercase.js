6398fc80eedffb8dc8cd70e8ef4cd7527611801b
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-07 14:50:17 +0300

    Update ex09.js

--- 04. Exercise Syntax, Functions and Statements/ex9_wordsUppercase.js
+++ 04. Exercise Syntax, Functions and Statements/ex9_wordsUppercase.js
@@ -1,3 +1,19 @@
+/* Write a program that extracts all words from a passed-in string and converts 
+them to upper case. The extracted words in the upper case must be printed on a 
+single line separated by ", ".
+The input comes as a single string argument - the text to extract and convert 
+words from.
+The output should be a single line containing the converted string
+Input                           Output 
+'Hi, how are you?'              HI, HOW, ARE, YOU
+
+Input                           Output
+'hello'                         HELLO
+
+Hints
+- You may need to use a Regular Expression or check for all delimiters that can be found in a sentence (ex. ",", 
+" ", "!", "?" and so on)   */
+
 function wordsUppercase(text){
     return words = text.match(new RegExp(/\w+/g)).join(", ").toUpperCase();
 }
