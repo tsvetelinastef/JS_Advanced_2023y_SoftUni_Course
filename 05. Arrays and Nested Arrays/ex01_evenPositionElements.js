8a72ea58e3581988ed535dbf2e0332aa2272c491
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-07 20:25:35 +0300

    Create ex01.js and ex02.js

---
+++ 05. Arrays and Nested Arrays/ex01_evenPositionElements.js
@@ -0,0 +1,21 @@
+/* Write a function that finds the elements at even positions in an array.
+The input comes as an array of string elements.
+The output is printed on the console. Collect all elements in a string, separated by space.
+Input                                       Output
+['20', '30', '40', '50', '60']              20 40 60
+['5', '10']                                 5
+*/
+function evenPositionElements(array) {
+    let result = '';
+
+    for (let i = 0; i < array.length; i += 2) {
+        result += array[i];
+        result += ' ';
+    }
+    console.log(result);
+}
+
+evenPositionElements(['20', '30', '40', '50', '60']);
+// 20 40 60
+evenPositionElements(['5', '10']);
+// 5
---
+++ 05. Arrays and Nested Arrays/ex02_lastKNumbersSequence.js
@@ -0,0 +1,8 @@
+function lastKNumbersSequence(){
+    
+}
+
+lastKNumbersSequence();
+// 
+lastKNumbersSequence();
+// 
