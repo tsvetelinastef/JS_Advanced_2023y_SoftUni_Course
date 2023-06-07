c1451802fe9fb5d6e09906671f6cf58b108714c7
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-07 23:35:12 +0300

    update of ex02 and creating ex03

--- 05. Arrays and Nested Arrays/ex02_lastKNumbersSequence.js
+++ 05. Arrays and Nested Arrays/ex02_lastKNumbersSequence.js
@@ -1,8 +1,32 @@
-function lastKNumbersSequence(){
-    
+/* You are given two integers n and k. Write a JS function that generates and
+ return the following sequence:
+ The first element is 1
+ Every following element equals the sum of the previous k elements
+ The length of the sequence is n elements
+
+The input comes as two number arguments. The first element represents the
+ number n, and the second – the number k.
+The output is the return value of your function and should be an array of 
+numbers.
+----------------------------------------------------------------------------
+Input       Output                  |  Input    Output                      |
+6, 3        [1, 1, 2, 4, 7, 13]     |  8, 2     [1, 1, 2, 3, 5, 8, 13, 21]  |
+
+Explanation
+The 2nd element (1) is the sum of the 3 elements before it, but there is only 1, so we take that. The third element is 
+the sum of the first 2 (1 and 1) and the 4th – the sum of 1, 1, and 2. The 5th element is the sum of the 2nd, 3rd, and 4th
+(1, 2, and 4) and so on */
+
+function lastKNumbersSequence(n, k) {
+    let firstElement = [1];
+    while (firstElement.length < n) {
+		let lastKnumbers = firstElement.slice(-k);
+		firstElement.push(lastKnumbers.reduce((acc, val) => acc + val, 0));
+	}
+	return firstElement;
 }
 
-lastKNumbersSequence();
-// 
-lastKNumbersSequence();
-// 
+console.log(lastKNumbersSequence(6, 3)); 
+// [1, 1, 2, 4, 7, 13] 
+console.log(lastKNumbersSequence(8, 2));
+// [1, 1, 2, 3, 5, 8, 13, 21]
---
+++ 05. Arrays and Nested Arrays/ex03_sumFirstLast.js
@@ -0,0 +1,20 @@
+/* Write a function that calculates and returns the sum of the first and 
+the last elements in an array.
+The input comes as an array of string elements holding numbers.
+The output is the return value of your function and should be a number
+Input                   Output              Input           Output
+['20', '30', '40']       60                  ['5', '10']     15
+*/
+
+/*function firstSolution(arr) {
+    let first = Number(arr[0]);
+    let last = Number(arr[arr.length - 1]);
+
+    return first + last;
+} */
+
+function secondSolution(arr) {
+    return Number(arr.shift()) + Number(arr.pop());
+}
+
+secondSolution(["20", "30", "40"]);
