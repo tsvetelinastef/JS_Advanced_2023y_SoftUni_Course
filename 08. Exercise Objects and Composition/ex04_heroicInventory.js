0cda3179275d5d8e95ee478c08ec3cbdac5f206a
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-13 00:36:30 +0300

    creating new files in folder Exer Objects n Compo
    creating ex01, ex02, ex03, ex04

---
+++ 08. Exercise Objects and Composition/ex04_heroicInventory.js
@@ -0,0 +1,20 @@
+function heroicInventory(array) {
+
+    let res = [];
+
+    array.forEach((line) => {
+        let [name, level, items] = line.split(" / ");
+        level = Number(level);
+        items = items ? items.split(", ") : [];
+        res.push({ name, level, items });
+    });
+    console.log(JSON.stringify(res));
+}
+
+heroicInventory([
+    "Isacc / 25 / Apple, GravityGun",
+    "Derek / 12 / BarrelVest, DestructionSword",
+    "Hes / 1 / ",
+]);
+// [{"name":"Isacc","level":25,"items":["Apple","GravityGun"]},{"name":"Derek","level":12,"items":["BarrelVest","DestructionSword"]},{"name":"Hes","level":1,"items":[]}]
+
