b70bfbee8d8b02b2b3ac4fe74f6c4cbf48d3422f
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-13 01:21:03 +0300

    DOM new files

--- 09. DOM Introduction/04. JS-Advanced-DOM-Intro_Lab_Resources/01.Edit Element/EditElement.js
+++ 09. DOM Introduction/04. JS-Advanced-DOM-Intro_Lab_Resources/01.Edit Element/EditElement.js
@@ -1,3 +1,6 @@
-function editElement() {
-    // TODO
+function editElement(ref, match, replacer) {
+    const text = ref.textContent;
+    const matcher = new RegExp(match, "g");
+    const changed = text.replace(matcher, replacer);
+    ref.textContent = changed;
 }
