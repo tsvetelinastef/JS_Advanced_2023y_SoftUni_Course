a0fb354fe8bbecbfe295a87ac4861bf6548f8e42
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-13 17:51:44 +0300

    adding a new file in DOM Intro Lab
    file called "showMore.js" ... if you run it you can't see anything in the console,
     because the essential changes are only visible in the browser

--- 09. DOM Introduction/04. JS-Advanced-DOM-Intro_Lab_Resources/04.Show More/ShowMore.js
+++ 09. DOM Introduction/04. JS-Advanced-DOM-Intro_Lab_Resources/04.Show More/ShowMore.js
@@ -1,3 +1,11 @@
 function showText() {
     // TODO
+    let hidden = document.getElementById("more");
+	let textToShow = document.getElementById("text");
+
+	textToShow.style.display = "inline";
+	hidden.style.display = "none";
 }
+
+// if you run it you can't see anything in the console,
+// because the essential changes are only visible in the browser
