84e36e1166f102469bb9fdc10fdf73d3750ec81e
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-14 01:07:52 +0300

    Update script.js
    ex01 from DOM manipulation and events Exercise

--- 12. Exercise DOM Manipulations and Events/JS_DOM_events_Exercise_Resources/01. Sections/script.js
+++ 12. Exercise DOM Manipulations and Events/JS_DOM_events_Exercise_Resources/01. Sections/script.js
@@ -1,16 +1,37 @@
+//function create(words) {
+//   const n = words.length;
+//   const dp = Array.from({ length: n }, () => Array.from({ length: n }, () => 0));
+
+//   const dp2 = Array.from({ length: n }, () => Array.from({ length: n }, () => 0));
+
+// dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1)
+
+
+
+// dp2[i][j] = min(dp2[i - 1][j - 1] + 1, dp2[i - 1][j] + 1, dp2[i][j - 1] + 1)
+
+
+
+//}
+
+
 function create(words) {
-   const n = words.length;
-   const dp = Array.from({ length: n }, () => Array.from({ length: n }, () => 0));
+   for (let word of words) {
+      let div = document.createElement("div");
+      let p = document.createElement("p");
+      p.innerText = word;
+      p.style.display = "none";
+      div.appendChild(p);
+      div.addEventListener("click", showP);
+      document.getElementById("content").appendChild(div);
+   }
 
-   const dp2 = Array.from({ length: n }, () => Array.from({ length: n }, () => 0));
-
-   // dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1)
-
-
-
-   // dp2[i][j] = min(dp2[i - 1][j - 1] + 1, dp2[i - 1][j] + 1, dp2[i][j - 1] + 1)
-
-
-   
+   function showP(event) {
+      if (event.target.nodeName === "P") {
+         return;
+      }
+      let p = event.target.children[0];
+      p.style.display = "block";
+   }
 }
 
