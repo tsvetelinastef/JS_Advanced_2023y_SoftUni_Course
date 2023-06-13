751b65fa3421f37d4c393feb63e5ff38eb732d18
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-13 17:54:39 +0300

    Update ColorizeTable.js
    in DOM Intro LAB

--- 09. DOM Introduction/04. JS-Advanced-DOM-Intro_Lab_Resources/05.Colorize Table/ColorizeTable.js
+++ 09. DOM Introduction/04. JS-Advanced-DOM-Intro_Lab_Resources/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,9 @@
 function colorize() {
-    // TODO
+    let table = document.querySelectorAll("tr");
+
+	for (let i = 0; i < table.length; i++) {
+		if (i % 2 !== 0) {
+			table[i].style.backgroundColor = "Teal";
+		}
+	}
 }
