67e9f4eb2f96a46d96707270a5ed98da825b0859
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-14 10:15:45 +0300

    Update script.js
    from folder Exercise Advanced Functions

--- 14. Exercise Advanced Functions/08. Next Article/script.js
+++ 14. Exercise Advanced Functions/08. Next Article/script.js
@@ -1,3 +1,16 @@
 function getArticleGenerator(articles) {
-    // TODO
+    // capture elements
+    const output = document.getElementById("content");
+    const ourCopy = articles.slice();
+
+    return function showNext() {
+        if (ourCopy[0] !== undefined) {
+            const article = document.createElement("article");
+            article.innerHTML = ourCopy.shift();
+            output.appendChild(article);
+        }
+
+        return showNext;
+    };
 }
+
