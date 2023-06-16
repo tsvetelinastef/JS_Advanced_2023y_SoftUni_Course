39622305acfee23d4b21c46ee4fc9be09f4d1f12
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-16 18:57:11 +0300

    Update ex07_sortingNumbers.js
    from folder Exercise Arrays and Nested Arrays

--- 06. Exercise Arrays and Nested Arrays/ex07_sortingNumbers.js
+++ 06. Exercise Arrays and Nested Arrays/ex07_sortingNumbers.js
@@ -1,3 +1,8 @@
+/* Write a function that sorts an array of numbers so that the first element
+is the smallest one, the second is the biggest one, the third is
+the second smallest one, the fourth is the second biggest one, and so on.
+Return the resulting array. */
+
 function sortingNumbers(array){
     let res = [];
 	array.sort((a, b) => a - b);
