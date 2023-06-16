e57451cbc07a3798529a47091dd31cf7ec65a72a
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-17 00:05:53 +0300

    Update ex11_diagonalAttack.js
    from folder: Exercise Arrays and Nested Arrays Lab

--- 06. Exercise Arrays and Nested Arrays/ex11_diagonalAttack.js
+++ 06. Exercise Arrays and Nested Arrays/ex11_diagonalAttack.js
@@ -1,3 +1,16 @@
+/* Write a JS function that reads a given matrix of numbers and checks
+if both main diagonals have equal sums. If they do, set every element that
+is NOT part of the main diagonals to that sum, alternatively just print 
+the matrix unchanged.
+The input comes as an array of strings. Each element represents a string of
+numbers, with spaces between them. 
+Parse it into a matrix of numbers, so you can work with it.
+The output is either the new matrix, with all cells not belonging to a main 
+diagonal are changed to the diagonal sum,
+or the original matrix if the two diagonals have different sums. You need to
+print every row on a new line, with cells separated by a space. 
+Check the examples below.  */
+
 function diagonalAttack(matrixString){
 
     let diagonalLeftRight = 0;
