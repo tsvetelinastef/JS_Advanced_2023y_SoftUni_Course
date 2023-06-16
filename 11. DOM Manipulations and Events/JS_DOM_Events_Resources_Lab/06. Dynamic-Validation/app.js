6fce6263072d12fda5a46866642e69dbd083e497
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-16 12:47:05 +0300

    Update app.js
    in folder: DOM Manipulation and Events Lab

--- 11. DOM Manipulations and Events/JS_DOM_Events_Resources_Lab/06. Dynamic-Validation/app.js
+++ 11. DOM Manipulations and Events/JS_DOM_Events_Resources_Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,20 @@
 function validate() {
-    console.log('TODO:...');
-}
+    const field = document.getElementById('email');
+  
+    field.addEventListener('change', (event) => {
+      if (validateEmail(field.value)) {
+        event.target.classList.remove('error');
+      } else {
+        event.target.classList.add('error');
+      }
+    });
+  
+    function validateEmail(email) {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return true;
+      }
+  
+      return false;
+    }
+  }
+  
