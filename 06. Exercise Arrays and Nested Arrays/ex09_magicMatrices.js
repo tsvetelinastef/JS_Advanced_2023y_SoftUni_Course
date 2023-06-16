1d636e0f025c1cf25f6a9fde84b2083de5fd6d1e
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-16 23:04:44 +0300

    Update ex09_magicMatrix.js
    from folder: Exercise Arrays and Nested Arrays

--- 06. Exercise Arrays and Nested Arrays/ex09_magicMatrices.js
+++ 06. Exercise Arrays and Nested Arrays/ex09_magicMatrices.js
@@ -1,3 +1,11 @@
+/* Write a function that checks if a given matrix of numbers is magical.
+A matrix is magical if the sums of the cells of every row and every column
+are equal. 
+The input comes as an array of arrays, containing numbers (number 2D matrix).
+The input numbers will always be positive.
+The output is a Boolean result indicating whether the matrix is magical or
+ not */
+
 function magicMatrices(matrix){
     for (let i = 0; i < matrix.length - 1; i++) {
 		let sumRowOne = matrix[i].reduce((acc, el) => acc + el);
