7b8cbb2a445f6e6754aafc980fd6de198c51dbaf
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-16 16:09:05 +0300

    Update ex04_rotateArray.js

--- 06. Exercise Arrays and Nested Arrays/ex04_rotateArray.js
+++ 06. Exercise Arrays and Nested Arrays/ex04_rotateArray.js
@@ -1,3 +1,14 @@
+/* 
+Write a JS function that rotates an array. The array should be rotated to 
+the right side, meaning that the last element should become the first,
+upon rotation. 
+The input comes as two parameters – an array of strings and a number. 
+The second parameter is the amount of rotation you need to perform.
+The output is the resulting array after the rotations. The elements 
+should be printed on one line, separated by a single space
+*/
+
+
 function rotateArray(array, step){
     for (let i = 0; i < step; i++) {
 		array.unshift(array.pop());
