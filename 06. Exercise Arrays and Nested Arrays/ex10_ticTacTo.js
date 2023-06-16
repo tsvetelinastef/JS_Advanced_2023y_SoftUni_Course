ce3281862ce85bee25b8709a5b1208858701942c
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-17 00:03:12 +0300

    Update ex10_ticTacToe.js
    from folder:  Exercise Arrays and Nested Arrays Lab

--- 06. Exercise Arrays and Nested Arrays/ex10_ticTacTo.js
+++ 06. Exercise Arrays and Nested Arrays/ex10_ticTacTo.js
@@ -1,3 +1,16 @@
+/* Make a tic-tac-toe console application.
+You will receive an array of arrays. As you know there are two players 
+in this game, so the first element of the input will be the first player's
+chosen coordinates, the second element will be the second player's turn 
+coordinates, and so on.
+The initial state of the dashboard is
+			[[false, false, false],
+			[false, false, false],
+			[false, false, false]]
+The first player's mark is X and the second player's mark is O.
+
+*/
+
 function ticTacTo(array){
     
     let initialDashboard = [
