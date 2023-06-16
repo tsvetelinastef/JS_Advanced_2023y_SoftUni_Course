f3b213421cc3ce39906ea5d6d67b778bb90c75af
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-06-16 21:16:16 +0300

    Update ex08_sortArray2Criteria
    from folder: Exercise Arrays and Nested Arrays Lab

--- 06. Exercise Arrays and Nested Arrays/ex08_sortArray2Criteria.js
+++ 06. Exercise Arrays and Nested Arrays/ex08_sortArray2Criteria.js
@@ -1,3 +1,11 @@
+/* Write a function that orders a given array of strings, by a length 
+in ascending order as primary criteria, and by alphabetical value 
+in ascending order as second criteria.
+The comparison should be case-insensitive.
+The input comes as an array of strings.
+The output is the elements of the ordered array of strings,
+printed each on a new line */
+
 function sortArray2Criteria(array){
     return array.sort((a, b) => {
         return a.length - b.length || a.localeCompare(b);
